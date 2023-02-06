8b28ec2685edba11120e2b7ddae6a92178eac129
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-06 11:21:46 +0900

    Set get request's reponse

--- src/server.js
+++ src/server.js
@@ -4,7 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", () => console.log("Somebody is trying to go home."));
+const handleHome = (req, res) => {
+    return res.send("Here it works!"); 
+}
+const handleLogin = (req, res) => {
+    return res.send("You can login here.");
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`); 
 
