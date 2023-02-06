b3dfbc0630c6861626619e1c962d22df6f29a070
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-06 17:14:41 +0900

    Make middleware

--- src/server.js
+++ src/server.js
@@ -4,15 +4,29 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+const banProtected = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>You cannot be here</h1>");
+    }
+    console.log("You can be here");
+    next();
+}
 const handleHome = (req, res) => {
     return res.send("Here it works!"); 
 }
-const handleLogin = (req, res) => {
-    return res.send("You can login here.");
+const protectedController = (req, res) => {
+    return res.send("Here is private");
 }
 
+app.use(logger);
+app.use(banProtected);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", protectedController);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`); 
 
