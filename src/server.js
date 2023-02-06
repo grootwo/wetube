7dfa748d59e9ca893b2e8c4b702efa64e0df20f0
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-06 11:06:23 +0900

    Fix POST variable name

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 // const express = require("express");
-const POST = 4000;
+const PORT = 4000;
+
 const app = express();
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`); 
 
-app.listen(POST, handleListening);
+app.listen(PORT, handleListening);
