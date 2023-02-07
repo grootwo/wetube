4b658b8dc49d2b374079389dc5111dddfbb04673
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-07 11:28:12 +0900

    Use morgan as a middleware

--- src/server.js
+++ src/server.js
@@ -1,32 +1,18 @@
-import express from "express";
 // const express = require("express");
+import express from "express";
+import morgan from "morgan";
+
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-const banProtected = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>You cannot be here</h1>");
-    }
-    console.log("You can be here");
-    next();
-}
 const handleHome = (req, res) => {
     return res.send("Here it works!"); 
 }
-const protectedController = (req, res) => {
-    return res.send("Here is private");
-}
 
 app.use(logger);
-app.use(banProtected);
 app.get("/", handleHome);
-app.get("/protected", protectedController);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`); 
 
