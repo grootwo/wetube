a66a1911de24210c8f53cd58124057aa646beb49
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-08 22:24:33 +0900

    Return to previous code

--- src/server.js
+++ src/server.js
@@ -1,39 +1,19 @@
+// const express = require("express");
 import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const urlLogger = (req, res, next) => {
-    console.log("Path: ", req.url);
-    next();
-};
-const timeLogger = (req, res, next) => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    console.log(`Time: ${year}/${month}/${day}`);
-    next();
-};
-const securityLogger = (req, res, next) => {
-    if (req.protocol === "https") {
-        console.log("Secure");
-    } else {
-        console.log("Insecure");
-    }
-    next();
-};
-const protecter = (req, res, next) => {
-    if (req.url === "/protected") {
-        return res.send("You are not allowed to enter");
-    } else {
-        next();
-    }
+const handleHome = (req, res) => {
+    return res.send("Here it works!"); 
 }
 
-app.use(protecter);
-app.use(urlLogger, timeLogger, securityLogger);
-app.get("/", (req, res) => res.send("<h1>Home</h1>"));
-app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
+app.use(logger);
+app.get("/", handleHome);
 
-// Codesandbox gives us a PORT :)
-app.listen(4000, () => `Listening!✅`);
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`); 
+
+app.listen(PORT, handleListening);
