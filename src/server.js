e2b6a7cb2bb69d51eba9a3e548d6d653af948ba7
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-09 13:36:55 +0900

    Make routers and use them

--- src/server.js
+++ src/server.js
@@ -6,13 +6,37 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
+
+const globalRouter = express.Router();
 
 const handleHome = (req, res) => {
-    return res.send("Here it works!"); 
+    return res.send("Home");
 }
 
-app.use(logger);
-app.get("/", handleHome);
+globalRouter.get("/", handleHome)
+
+const usersRouter = express.Router();
+
+const handleUserEdit = (req, res) => {
+    return res.send("User edit");
+}
+
+usersRouter.get("/edit", handleUserEdit);
+
+const videosRouter = express.Router();
+
+const handleVideoWatch = (req, res) => {
+    return res.send("Video watch");
+}
+
+videosRouter.get("/watch", handleVideoWatch);
+
+
+app.use("/", globalRouter);
+app.use("/users", usersRouter);
+app.use("/videos", videosRouter);
+
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`); 
 
