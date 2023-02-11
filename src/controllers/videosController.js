0d45b999c7c68f7ef3d2620f12ebcca52b38d7ea
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-11 17:56:39 +0900

    Make videos obj and show by using mixin

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -4,7 +4,32 @@ const fakeUser = {
 }
 
 export const trending = (req, res) => {
-    const videos = []
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+    ];
     return res.render("home", {pageTitle: "Home", fakeUser: fakeUser, videos: videos});
 }
 export const watch = (req, res) => {
