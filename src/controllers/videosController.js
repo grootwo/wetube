94ee1df16a7929275fec4fcebf09e16cf8e58ddb
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-13 14:27:28 +0900

    Make uploading page

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -44,3 +44,9 @@ export const postEdit = (req, res) => {
     videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 }
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle: "Uploading video"});
+}
+export const postUpload = (req, res) => {
+    return res.redirect("/");
+}
--- src/routers/videosRouter.js
+++ src/routers/videosRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { watch, getEdit, postEdit} from "../controllers/videosController";
+import { watch, getEdit, postEdit, getUpload, postUpload} from "../controllers/videosController";
 
 const videosRouter = express.Router();
 
 videosRouter.get("/:id(\\d+)", watch);
 videosRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videosRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videosRouter;
