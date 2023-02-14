7a6049498cdb6ab724e653aeec762d9b2d3fcd69
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-14 11:03:06 +0900

    Remove fake datas and change trending -> home

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,47 +1,17 @@
-const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 1,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 2,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 4399,
-            id: 3,
-        },
-];
-
-export const trending = (req, res) => {
+export const homr = (req, res) => {
     return res.render("home", {pageTitle: "Home", videos: videos});
 }
 export const watch = (req, res) => {
     const id = req.params.id;
-    const video = videos[id - 1]
-    return res.render("watch", {pageTitle: `Watching ${video.title} video`, video: video});
+    return res.render("watch", {pageTitle: `Watching video` });
 }
 export const getEdit = (req, res) => {
     const id = req.params.id;
-    const video = videos[id - 1];
-    return res.render("edit", {pageTitle: `Editing ${video.title} video`, video: video});
+    return res.render("edit", {pageTitle: `Editing video` });
 }
 export const postEdit = (req, res) => {
     const id = req.params.id;
     const title = req.body.title;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 }
 export const getUpload = (req, res) => {
@@ -49,14 +19,5 @@ export const getUpload = (req, res) => {
 }
 export const postUpload = (req, res) => {
     const { title } = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "Just now",
-        views: 0,
-        id: videos.length + 1,
-    }
-    videos.push(newVideo);
     return res.redirect("/");
 }
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { join, login } from "../controllers/usersController";
-import { trending } from "../controllers/videosController";
+import { home } from "../controllers/videosController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending);
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
