4d73ba5b8c1689a3a34b431591207cdc331fe249
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-16 19:16:48 +0900

    Set property of video

--- src/model/Video.js
+++ src/model/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    title: { type: String, required: true, trim: true, maxLength: 30 },
+    description: { type: String, required: true, trim: true, maxLength: 100 },
     createdAt: {type: Date, required: true, default: Date.now },
-    hashtags: [{ type: String }],
+    hashtags: [{ type: String, required: true, trim: true }],
     meta: {
         views: { type: Number, required: true, default: 0},
         rating: { type: Number, required: true, default: 0},
