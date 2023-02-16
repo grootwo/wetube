39c12e72502d702b8ee270f86de3059b8dd55b0d
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-16 18:38:44 +0900

    Save and Upload new video

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -24,7 +24,7 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Uploading video"});
 }
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const { title, description, hashtags } = req.body;
     const video = new Video({
         title,
@@ -36,6 +36,6 @@ export const postUpload = (req, res) => {
             rating: 0,
         }
     });
-    console.log(video);
+    await video.save();
     return res.redirect("/");
 }
