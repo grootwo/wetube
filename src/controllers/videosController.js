31a0ebccee2bc3d2391f21fd35f5b26d695d681e
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-17 15:21:49 +0900

    Add save hashtags middleware

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -34,7 +34,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
         title,
         description,
-        hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
+        hashtags,
     })
     return res.redirect(`/videos/${id}`);
 }
