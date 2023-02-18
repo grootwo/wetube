ce0f318e3d1c98fb58d03c6a8e8d797171cb9908
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-18 15:50:32 +0900

    Add func delete video

--- src/routers/videosRouter.js
+++ src/routers/videosRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload} from "../controllers/videosController";
+import { 
+    watch, 
+    getEdit, 
+    postEdit, 
+    getUpload, 
+    postUpload, 
+    deleteVideo 
+} from "../controllers/videosController";
 
 const videosRouter = express.Router();
 
 videosRouter.route("/upload").get(getUpload).post(postUpload);
 videosRouter.get("/:id([0-9a-f]{24})", watch);
 videosRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videosRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 
 export default videosRouter;
