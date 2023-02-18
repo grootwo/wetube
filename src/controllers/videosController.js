6e2cb5e9668a67122165cd8243dbd5e89772c8fa
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-18 16:14:58 +0900

    Add searching func

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -60,3 +60,7 @@ export const deleteVideo = async (req, res) => {
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
 }
+export const search = (req, res) => {
+    const { keyword } = req.query;
+    return res.render("search", { pageTitle: "Search" });
+}
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { join, login } from "../controllers/usersController";
-import { home } from "../controllers/videosController";
+import { home, search } from "../controllers/videosController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
