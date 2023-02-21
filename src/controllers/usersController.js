2d465250b5a958547ed0602e2d0c6cf344a4fc93
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-21 10:04:28 +0900

    Add loggedin session

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -45,6 +45,8 @@ export const postLogin = async (req, res) => {
       errorMsg: "Wrong password",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const edit = (req, res) => {
