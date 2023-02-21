d12894f9314f668b47a36cd4b36a8c8a5b3e09af
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-21 16:22:09 +0900

    Hide some info by using .env

--- src/db.js
+++ src/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
-mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 
 const handleError = (error) => {
-    console.log("❗️DB error: ", error);
-}
+  console.log("❗️DB error: ", error);
+};
 const handleOpen = () => {
-    console.log("✅DB is connected");
-}
+  console.log("✅DB is connected");
+};
 
 db.on("error", handleError);
 db.once("open", handleOpen);
