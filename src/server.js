1d657d33176b202d39419b78291156ae813e6767
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-21 10:39:01 +0900

    Check loggedin and show li
    Using locals

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import usersRouter from "./routers/usersRouter";
 import videosRouter from "./routers/videosRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -21,13 +22,7 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  });
-});
-
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", usersRouter);
 app.use("/videos", videosRouter);
