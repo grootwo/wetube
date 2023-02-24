cb6fcd2458c3d4319fc604b7f937d95ac117432d
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-24 14:27:10 +0900

    Make post edit
    Info does not change

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -135,8 +135,20 @@ export const logout = (req, res) => {
 export const getEdit = (req, res) => {
   return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
-export const postEdit = (req, res) => {
-  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { email, name, avatarUrl, location },
+  } = req;
+  await User.findByIdAndUpdate(_id, {
+    email,
+    name,
+    avatarUrl,
+    location,
+  });
+  return res.render("edit-profile");
 };
 export const remove = (req, res) => {
   return res.send("User remove");
