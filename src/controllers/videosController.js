f0e5168134e1399b7691bcb56c3601628e2dca0a
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-25 17:27:51 +0900

    Add func uploading video

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -42,10 +42,12 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Uploading video" });
 };
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
+      fileUrl,
       description,
       hashtags: Video.formatHashtags(hashtags),
     });
--- src/model/Video.js
+++ src/model/Video.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: { type: String, required: true, trim: true, maxLength: 30 },
-    description: { type: String, required: true, trim: true, maxLength: 100 },
-    createdAt: {type: Date, required: true, default: Date.now },
-    hashtags: [{ type: String, required: true, trim: true }],
-    meta: {
-        views: { type: Number, required: true, default: 0},
-        rating: { type: Number, required: true, default: 0},
-    }
+  title: { type: String, required: true, trim: true, maxLength: 30 },
+  fileUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, maxLength: 100 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, required: true, trim: true }],
+  meta: {
+    views: { type: Number, required: true, default: 0 },
+    rating: { type: Number, required: true, default: 0 },
+  },
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
-    return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
