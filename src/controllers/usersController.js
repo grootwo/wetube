8ce43075e43f4fe5d54d56aa355434d1faca88fa
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-25 20:17:25 +0900

    Show videos in profile using populate

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -192,12 +192,11 @@ export const postChangePw = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
-  const videos = await Video.find({ owner: id });
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found" });
   }
-  return res.render("profile", { pageTitle: user.name, user, videos });
+  return res.render("profile", { pageTitle: user.name, user });
 };
 
 export const remove = (req, res) => {
