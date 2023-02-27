e895b20e6ba48be155a36f122eb7a4c634469c08
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-27 10:58:32 +0900

    Set webpack.config.js output absolute path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/client/js/main.js",
   output: {
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
   },
 };
