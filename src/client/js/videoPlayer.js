9bcea0c350595a179c4caff71e235705db92c523
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-28 17:22:46 +0900

    Edit playtime

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,8 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -36,6 +38,16 @@ const handleVolumeInput = (e) => {
   video.volume = changedValue;
 };
 
+const handleVideoLoadedmetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handletVideoTimeupdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeInput);
+video.addEventListener("timeupdate", handletVideoTimeupdate);
+video.addEventListener("loadedmetadata", handleVideoLoadedmetadata);
