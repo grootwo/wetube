ed3d7e0b400e0171d24840a22755263be765541b
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-02 11:18:41 +0900

    Make preview

--- src/client/js/videoRecorder.js
+++ src/client/js/videoRecorder.js
@@ -1,12 +1,15 @@
 const recordBtn = document.getElementById("recordBtn");
+const videoPreview = document.getElementById("preview");
 
 const handleRecordClick = async () => {
   console.log("record");
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true,
+    video: { width: 600, height: 450 },
   });
   console.log(stream);
+  videoPreview.srcObject = stream;
+  videoPreview.play();
 };
 
 recordBtn.addEventListener("click", handleRecordClick);
