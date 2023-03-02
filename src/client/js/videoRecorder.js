da93a7d1ed9b7cd46265f6680e9c061df9f8d4f2
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-02 11:50:09 +0900

    Make recorder by using media recorder

--- src/client/js/videoRecorder.js
+++ src/client/js/videoRecorder.js
@@ -1,9 +1,35 @@
 const recordBtn = document.getElementById("recordBtn");
 const videoPreview = document.getElementById("preview");
 
-const handleRecordClick = async () => {
-  console.log("record");
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleRecordStart = () => {
+  recordBtn.innerText = "Stop Record";
+  recordBtn.removeEventListener("click", handleRecordStart);
+  recordBtn.addEventListener("click", handleRecordEnd);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log("recording start");
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const handleRecordEnd = () => {
+  recordBtn.innerText = "Start Record";
+  recordBtn.removeEventListener("click", handleRecordEnd);
+  recordBtn.addEventListener("click", handleRecordStart);
+};
+
+const setPreview = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: { width: 600, height: 450 },
   });
@@ -12,4 +38,6 @@ const handleRecordClick = async () => {
   videoPreview.play();
 };
 
-recordBtn.addEventListener("click", handleRecordClick);
+recordBtn.addEventListener("click", handleRecordStart);
+
+setPreview();
