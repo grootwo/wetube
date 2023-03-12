b4df8408bb140c10313bd4506a6b625a9b540d4f
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-13 03:56:43 +0900

    Delete / before image's url

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -170,7 +170,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? `/${file.location}` : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       email,
       name,
       location,
